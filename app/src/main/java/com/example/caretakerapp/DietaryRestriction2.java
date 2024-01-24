aefb27afcca0b5d19252b8de87d54b8e75caa290
Ye-june <Peterkim7290@gmail.com>
2024-01-24 18:35:38 -0500

    Nega

--- app/src/main/java/com/example/caretakerapp/DietaryRestriction2.java
+++ app/src/main/java/com/example/caretakerapp/DietaryRestriction2.java
@@ -24,3 +24,4 @@ public class DietaryRestriction2 extends AppCompatActivity {
         receiver_msg.setText(str);
     }
 }
+
--- app/src/main/java/com/example/caretakerapp/LocationTracking.java
+++ app/src/main/java/com/example/caretakerapp/LocationTracking.java
@@ -5,32 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationTracking extends AppCompatActivity implements OnMapReadyCallback {
+public class LocationTracking extends AppCompatActivity {
 
-    private GoogleMap myMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_tracking);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
-        myMap = googleMap;
-
-        LatLng sydney = new LatLng(-34,151);
-        myMap.addMarker(new MarkerOptions().position(sydney).title("Sydney"));
-        myMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
